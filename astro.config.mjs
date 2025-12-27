36b04c9b47140db8674240fa451ece4320e11ce1
Dominik Mazura <mazura@shoptet.cz>
2025-12-27 15:52:44 +0100

    Deploy Astro to GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,6 @@ import { defineConfig } from "astro/config";
 
 export default defineConfig({
   output: "static",
+  site: "https://hell.dominikmazura.cz",
+  base: process.env.ASTRO_BASE ?? "/",
 });
-
